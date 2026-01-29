da8681aa7cdfc12b2161f90b9596723b93a17334
Lya603 <ainhoasanchez245@gmail.com>
2026-01-29 12:36:31 +0100

    primer commit

--- backend/src/main/java/com/smartcart/smartcart/modules/category/entity/Category.java
+++ backend/src/main/java/com/smartcart/smartcart/modules/category/entity/Category.java
@@ -1,5 +1,34 @@
 package com.smartcart.smartcart.modules.category.entity;
 
-public class Category {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Table(name = "category")
+@Data 
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int categoryId;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    //empty constructor
+    public Category() {}
+
+    //full constructor
+    public Category(int categoryId, String name, String description) {
+        this.categoryId = categoryId;
+        this.name = name;
+        this.description = description;
+    }
+    
 }
--- backend/src/main/java/com/smartcart/smartcart/modules/product/entity/Product.java
+++ backend/src/main/java/com/smartcart/smartcart/modules/product/entity/Product.java
@@ -1,5 +1,68 @@
 package com.smartcart.smartcart.modules.product.entity;
 
+
+import org.springframework.data.annotation.Id;
+
+import com.smartcart.smartcart.modules.category.entity.Category;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "product")
+@Data
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Integer productId;
+
+    @ManyToOne
+    @JoinColumn(name = "id_category")
+    private Category categoryId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "ean") //código de barras
+    private String ean;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "quantity")
+    private Double quantity;
+
+    @Column(name = "unit") // unidad de medida
+    private String unit;
+
+    //constructors
+    public Product() {}
+
+    public Product(Integer productId, Category categoryId, String name, String ean, String brand, String description,
+            String imageUrl, Double quantity, String unit) {
+        this.productId = productId;
+        this.categoryId = categoryId;
+        this.name = name;
+        this.ean = ean;
+        this.brand = brand;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+    
 
 }
